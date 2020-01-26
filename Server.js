0daac8e83e10c91173aad5875d439e2ef1f21d0b
Andrew Mueller <iglooworks1505@gmail.com>
2020-01-26 11:03:42 -0600

    just added connection to mongo and literally nothing else. Going to make a ton of commits now since I lost an entire days work yesterday

---
+++ Server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+//!! DEFINE ROUTES here
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Successfully started on port ${PORT}`));
---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = () => {
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log("MongoDB Connected"));
+};
+
+module.exports = connectDB;
