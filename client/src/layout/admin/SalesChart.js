358bcda8820be77c05545a4f0bd669b2e2b3f423
Andrew Mueller <iglooworks1505@gmail.com>
2020-02-06 14:17:28 -0600

    messing with data input. Making progress, saving before this all falls apart over a semicolon

--- client/src/layout/admin/SalesChart.js
+++ client/src/layout/admin/SalesChart.js
@@ -1,8 +1,81 @@
 import React, { Component } from "react";
 import Chart from "react-apexcharts";
 
-const SalesChart = () => {
-  return <div></div>;
+const SalesChart = ({ ArrByDate, sumToday }) => {
+  console.log(ArrByDate);
+  let x = [];
+  let y = [];
+  for (var i = 0; i < ArrByDate.length; i++) {
+    y.push(ArrByDate[i].length);
+    let z = ArrByDate[i][0].slice(0, 3);
+    let q = ArrByDate[i][0].split(" ")[1];
+    x.push(z + " " + q);
+
+    // x.push(ArrByDate[i][0]);
+  }
+  console.log(x);
+  console.log(y);
+  let options = {
+    chart: {
+      id: "lineChart"
+    },
+    title: {
+      text: "Individual Sales by day",
+      align: "left"
+    },
+    chart: {
+      toolbar: {
+        show: false,
+        tools: {
+          download: true
+        },
+        autoSelected: "zoom"
+      }
+    },
+    annotations: {
+      yaxis: [
+        {
+          y: sumToday.length,
+          borderColor: "#00E396",
+          label: {
+            borderColor: "#00E396",
+            style: {
+              color: "#fff",
+              background: "#00E396"
+            },
+            text: "Today"
+          }
+        }
+      ]
+    },
+    xaxis: {
+      type: "category",
+      categories: x,
+      labels: {
+        rotateAlways: true,
+        show: true,
+        rotate: -45
+      }
+    }
+  };
+  let series = [
+    {
+      name: "Individual Sales",
+      data: y
+    }
+  ];
+
+  return (
+    <div>
+      <Chart
+        options={options}
+        series={series}
+        type="line"
+        width="100%"
+        style={{ padding: "20px", overflow: "show" }}
+      />
+    </div>
+  );
 };
 
 export default SalesChart;
--- client/src/layout/admin/pages/Admin.js
+++ client/src/layout/admin/pages/Admin.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Preloader from "../../Preloader";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import moment from "moment";
@@ -13,19 +14,20 @@ const Admin = ({ log: { logs, loading, filtered, sales }, getSales }) => {
   }, []);
 
   // Gets the sum quantity of sales, not individual sales
+  // !!Need to get this working again
   var sumArr = 0;
   // returns an array of whole sales objects for day retrieved
   let sumToday = [];
-  let dateCount;
-  let xAxis;
+  let x;
+  let ArrByDate;
+  let dateCount = [];
   const date = moment().format("MMMM Do YYYY");
   if (sales !== null && !loading) {
     const ret = sales.map(sale => sale.sale_date.split(",")[0]);
-    dateCount = _.groupBy(ret);
-    console.log(dateCount);
-    for (var x = 0; x < dateCount.length; x++) {
-      xAxis = Object.Keys(dateCount);
-    }
+    x = _.groupBy(ret);
+
+    dateCount.push(x);
+
     for (var i = 0; i < sales.length; i++) {
       const d = sales[i].sale_date.split(",")[0];
       if (d == date) {
@@ -33,18 +35,21 @@ const Admin = ({ log: { logs, loading, filtered, sales }, getSales }) => {
       }
       sumArr += sales[i].quantity;
     }
-
-    console.log(sales[1].sale_date.split(",")[0]);
-    console.log(dateCount);
-    console.log(`Daily sum: ${sumToday}`);
-    console.log(Object.keys(dateCount));
+    ArrByDate = Object.values(x);
   }
 
   return (
     <div>
       <h1>Admin stuff here:</h1>
-      <h5>Sales Quantity: {sales !== null ? sales.length : null} </h5>
-      <h5>Sales Total: {sales !== null ? <SalesChart /> : null} </h5>
+      {sales !== null ? (
+        <SalesChart
+          ArrByDate={ArrByDate}
+          sumToday={sumToday}
+          style={{ padding: "15px", margin: "auto" }}
+        />
+      ) : (
+        <Preloader />
+      )}
     </div>
   );
 };
