93c5999a29bc3971fab83a0cdcc28ff2023453cd
Andrew Mueller <iglooworks1505@gmail.com>
2020-02-09 10:33:34 -0600

    admin button conditionally displayed

--- client/src/layout/admin/AdminBtn.js
+++ client/src/layout/admin/AdminBtn.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-
+import React, { Fragment, useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-const AdminBtn = ({ loggedIn }) => {
+const AdminBtn = ({ employee: { user }, loggedIn }) => {
   const adminToggle = () => {
     let p = window.location.pathname;
     if (p == "/") {
@@ -11,26 +12,44 @@ const AdminBtn = ({ loggedIn }) => {
       return "/";
     }
   };
+  const { supervisor } = user;
 
   return (
-    <div className="fixed-action-btn" style={{ marginRight: "80px" }}>
-      <Link to={adminToggle} className="btn-floating btn-large red darken-2">
-        <i className="large material-icons">trending_up</i>
-      </Link>
-      <ul>
-        <li className="btn-floating green modal-trigger">
-          <a href="#add-emp-modal" className="modal-trigger">
-            <i className="material-icons">person_add</i>
-          </a>
-        </li>
-        <li className="btn-floating grey">
-          <a href="#employee-list-modal" className="modal-trigger">
-            <i className="material-icons">person</i>
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Fragment>
+      {supervisor ? (
+        <div className="fixed-action-btn" style={{ marginRight: "80px" }}>
+          <Link
+            to={adminToggle}
+            className="btn-floating btn-large red darken-2"
+          >
+            <i className="large material-icons">trending_up</i>
+          </Link>
+          <ul>
+            <li className="btn-floating green modal-trigger">
+              <a href="#add-emp-modal" className="modal-trigger">
+                <i className="material-icons">person_add</i>
+              </a>
+            </li>
+            <li className="btn-floating grey">
+              <a href="#employee-list-modal" className="modal-trigger">
+                <i className="material-icons">person</i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      ) : (
+        ""
+      )}
+    </Fragment>
   );
 };
 
-export default AdminBtn;
+const mapStateToProps = state => ({
+  employee: state.employee
+});
+
+AdminBtn.propTypes = {
+  employee: PropTypes.object.isRequired
+};
+
+export default connect(mapStateToProps)(AdminBtn);
